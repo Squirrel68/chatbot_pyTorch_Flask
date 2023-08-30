88f122ffb42c3168a7f49f21df2238b54e14cc7d
Squirrel68 <constantlypeacinside@gmail.com>
2023-08-30 16:46:00 +0700

    add flask_cors

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-@app.get("/")
-def index_get():
-    return render_template("base.html")
+# @app.get("/")
+# def index_get():
+#     return render_template("base.html")
 @app.post("/predict")
 def predict():
     text =request.get_json().get("message")
